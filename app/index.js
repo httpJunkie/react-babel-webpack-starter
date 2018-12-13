31e3c04dec792cfe7c9d0b4bca9194c4da751a7f
Eric Bishard <e.bishard@gmail.com>
2018-12-13 11:16:00 -0800

    add simple ProptTypes to require typed control of props for a component

--- app/index.js
+++ app/index.js
@@ -1,29 +1,47 @@
 import React from 'react';
 // import Todo from './todos/Todos';
+import PropTypes from 'prop-types';
 import ReactDOM from 'React-dom';
 import './index.css';
+
+
 
 class Badge extends React.Component {
   render() {
     return (
       <>
         <img
-          src={this.props.img}
+          src={this.props.img.profileImage}
           alt="Avatar"
           style={{ width: 100, height: 100 }}
         />
         <h2>Name: {this.props.name}</h2>
         <h3>Username: {this.props.username}</h3>
+        <h3>Clearance Levels:</h3>
+        <p>{this.props.clearance.map(
+          (item, idx) => {
+            let useKeyAlt = `${item}-${idx}`;
+            return <span key={useKeyAlt} alt={useKeyAlt} className="clearance-level">{item}</span>
+          }
+        )}</p>
       </>
     )
   }
+}
+
+Badge.propTypes = {
+  img: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  clearance: PropTypes.arrayOf(PropTypes.number)
 }
 
 ReactDOM.render(
   <Badge
     name="Eric Bishard"
     username="httpJunkie"
-    img="https://github.com/httpJunkie.png"
+    img={{ profileImage: "https://github.com/httpJunkie.png" }}
+    clearance={[1, 2, 3, 5, 8, 13, 21, 1]}
   />,
   document.getElementById("app")
 )
